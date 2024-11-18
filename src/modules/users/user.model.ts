477718c796966ba55b5bf773c1fc709bfee1d88d
Usaid Aqeel <usaidaqeel309@outlook.com>
2024-11-18 19:24:07 +0500

    update user.model.ts

--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -1,22 +1,15 @@
-import mongoose, { Document, ObjectId, Schema } from 'mongoose';
-import uniqueValidators from 'mongoose-unique-validator';
-import { SoftDeleteModel, softDeletePlugin } from 'soft-delete-plugin-mongoose';
+import mongoose, { Document, ObjectId, Schema } from "mongoose";
 
 export interface IUsers extends Document {
-    _id?: ObjectId;
-    email: string;
-    password?: string;
-  }
+  name: string;
+  email: string;
+  password: string;
+}
 
-  const UserSchema: Schema = new Schema({
-    name: { type: String, required: true, unique: false },
-    email: { type: String, required: false },
-    password: { type: String, required: false, default: '' },
-  })
+const UserSchema: Schema = new Schema({
+  name: { type: String, required: true, unique: false },
+  email: { type: String, required: false },
+  password: { type: String, required: false, default: "" },
+});
 
-    UserSchema.plugin(uniqueValidators);
-    UserSchema.plugin(softDeletePlugin);
-    export const User = mongoose.model<IUsers, SoftDeleteModel<IUsers>>(
-      'User',
-      UserSchema,
-    );
+export const User = mongoose.model<IUsers>("User", UserSchema);

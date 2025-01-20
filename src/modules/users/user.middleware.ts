8227455e909107ad2e59252c522bf2554c184861
Usaid Aqeel <usaidaqeel309@outlook.com>
2025-01-21 01:10:16 +0500

    chore: update the package.json

--- src/modules/users/user.middleware.ts
+++ src/modules/users/user.middleware.ts
@@ -18,3 +18,18 @@ export const userValidator = [
       next();
     },
   ];
+
+export const loginValidator = [
+  check('email', 'Please enter valid email address').isEmail(),
+  check('password', 'Please enter your password').not().isEmpty(),
+  (req: Request, res: Response, next: NextFunction): void => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      const err = new Error((error as any).errors[0].msg);
+      err.statusCode = httpStatusCodes.UNPROCESSABLE_ENTITY;
+      err.data = error;
+      return next(err);
+    }
+    next();
+  },
+]  
